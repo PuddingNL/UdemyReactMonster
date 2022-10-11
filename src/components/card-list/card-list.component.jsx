988972b3a2002ce6d456bda4a3245167180e61eb
Jeroen Bijl <jeroen.bijl@ordina.nl>
2022-10-11 13:31:37 +0200

    61 Convert to Functional Components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,18 +1,14 @@
-import { Component } from "react";
 import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-    return (
-      <section className="card-list">
-        {monsters.map((monster) => {
-          return <Card monster={monster} />;
-        })}
-      </section>
-    );
-  }
-}
+const CardList = ({ monsters }) => {
+  return (
+    <section className="card-list">
+      {monsters.map((monster) => {
+        return <Card monster={monster} />;
+      })}
+    </section>
+  );
+};
 
 export default CardList;
