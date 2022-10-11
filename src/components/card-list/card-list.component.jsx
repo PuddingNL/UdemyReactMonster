1b87e831500b4756cafafc3ff8dcf8feef75daa7
Jeroen Bijl <jeroen.bijl@ordina.nl>
2022-10-11 13:36:35 +0200

    Optimizing the components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,12 @@
 import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-const CardList = ({ monsters }) => {
-  return (
-    <section className="card-list">
-      {monsters.map((monster) => {
-        return <Card monster={monster} />;
-      })}
-    </section>
-  );
-};
+const CardList = ({ monsters }) => (
+  <section className="card-list">
+    {monsters.map((monster) => {
+      return <Card monster={monster} />;
+    })}
+  </section>
+);
 
 export default CardList;
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,15 +1,12 @@
 import "./search-box.styles.css";
 
-const SearchBox = (props) => {
-  const { onChangeHandler, placeholder, className } = props;
-  return (
-    <input
-      className={className}
-      type="search"
-      placeholder={placeholder}
-      onChange={onChangeHandler}
-    />
-  );
-};
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className={className}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
 
 export default SearchBox;
